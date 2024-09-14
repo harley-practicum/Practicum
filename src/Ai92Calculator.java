527ee8dd61610dead80c61bebd6ffc992f32e26b
harley-andrey <harley-andrey@yandex.com>
2024-09-14 16:48:00 +0300

    delete лишнее

--- src/Ai92Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Ai92Calculator {
-    double price = 42.74;
-    double tax = 0.13;
-    double fullPrice = price * (1 + tax);
-
-    public double calculate(double razmer) {
-        return razmer * fullPrice;
-    }
-}
--- src/Ai95Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Ai95Calculator {
-    double price = 46.35;
-    double tax = 0.14;
-    double fullPrice = price * (1 + tax);
-
-    public double calculate(double razmer) {
-        return razmer * fullPrice;
-    }
-}
