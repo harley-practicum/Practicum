f822882175529d69287c63833368f01da400db83
harley-andrey <harley-andrey@yandex.com>
2024-09-14 16:22:31 +0300

    razmer->volume

--- Pi/src/Pi.java
+++ Pi/src/Pi.java
@@ -1,19 +1,19 @@
 public class Pi {
     public static void main(String[] args) {
-        if(args == null || args.length == 0){
+        if (args == null || args.length == 0) {
             System.out.println("Введите количество пифагоровых троек для вывода или введите три числа для проверки.");
             return;
         }
-        if(args.length == 1){
+        if (args.length == 1) {
             int N = Integer.parseInt(args[0]);
             int a = 1;
             int i = 0;
-            while (i < N && a < 1000){
+            while (i < N && a < 1000) {
                 int b = 1;
                 while (i < N && b < 1000) {
                     int c = 1;
-                    while (i < N && c < 1000){
-                        if(checkTriple(a, b, c)){
+                    while (i < N && c < 1000) {
+                        if (checkTriple(a, b, c)) {
                             System.out.println("Числа " + a + ", " + b + ", " + c + " являются пифагоровой тройкой");
                             i++;
                         }
@@ -27,8 +27,8 @@ public class Pi {
             int a = Integer.parseInt(args[0]);
             int b = Integer.parseInt(args[1]);
             int c = Integer.parseInt(args[2]);
-            if(checkTriple(a, b, c)){
-                System.out.println(a + ", "+ b + ", "+ c + " — это настоящая пифагорова тройка!");
+            if (checkTriple(a, b, c)) {
+                System.out.println(a + ", " + b + ", " + c + " — это настоящая пифагорова тройка!");
             } else {
                 System.out.println("Эти числа не являются пифагоровой тройкой");
             }
